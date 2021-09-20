5a26b3ac9ca69da7fccf5d7a82bb52e9e5b3a482
Rohan-Saini7996 <rohan0211.cse19@chitkara.edu.in>
2021-09-21 01:04:06 +0530

    js of todoList

---
+++ app.js
@@ -0,0 +1,19 @@
+let addToButton=document.getElementById("addToDo");
+let toDoContainer=document.getElementById("toDoContainer");
+let inputField=document.getElementById("inputField");
+
+addToButton.addEventListener('click',function(){
+    var paragraph=document.createElement('p');
+    paragraph.innerHTML=inputField.value;
+    paragraph.classList.add('paragraph-styling');
+    toDoContainer.appendChild(paragraph);
+
+    inputField.value=" ";
+    paragraph.addEventListener('click',function(){
+        paragraph.style.textDecoration="line-through";
+    })
+    paragraph.addEventListener('dblclick',function(){
+        toDoContainer.removeChild(paragraph);
+    })
+
+})
